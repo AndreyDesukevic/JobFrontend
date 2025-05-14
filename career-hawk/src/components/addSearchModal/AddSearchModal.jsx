3fbb18aecfc8d44b3772c34dfe3e34ba0f81906d
AndreyDesukevic <andrejdesukevic@gmail.com>
2025-05-15 02:30:09 +0300

    Added modal window

---
+++ career-hawk/src/components/addSearchModal/AddSearchModal.jsx
@@ -0,0 +1,48 @@
+import { Button, CloseButton, Dialog, Portal, Field, Input, Stack } from "@chakra-ui/react"
+import { useRef } from "react"
+
+const AddSearchModal = () => {
+  const ref = useRef(null)
+  return (
+    <Dialog.Root initialFocusEl={() => ref.current}>
+      <Dialog.Trigger asChild>
+        <Button colorScheme="teal" size="md" borderRadius="xl" ml={8}>
+          + Создать поиск
+        </Button>
+      </Dialog.Trigger>
+      <Portal>
+        <Dialog.Backdrop /> {/* Добавьте эту строку для затемнения фона */}
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>Создать поиск</Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Body pb="4">
+              <Stack gap="4">
+                <Field.Root>
+                  <Field.Label>Название поиска</Field.Label>
+                  <Input placeholder="Введите название" />
+                </Field.Root>
+                <Field.Root>
+                  <Field.Label>Описание</Field.Label>
+                  <Input ref={ref} placeholder="Введите описание" />
+                </Field.Root>
+              </Stack>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Dialog.ActionTrigger asChild>
+                <Button variant="outline" mr={3}>Отмена</Button>
+              </Dialog.ActionTrigger>
+              <Button colorScheme="teal" colorPalette="green">Сохранить</Button>
+            </Dialog.Footer>
+            <Dialog.CloseTrigger asChild>
+              <CloseButton size="sm" position="absolute" right="8px" top="8px" />
+            </Dialog.CloseTrigger>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  )
+}
+
+export default AddSearchModal
--- career-hawk/src/pages/Searches.jsx
+++ career-hawk/src/pages/Searches.jsx
@@ -1,27 +1,23 @@
-import { Box, Heading, Text, Button, Flex } from "@chakra-ui/react"
+import { Box, Heading, Text, Flex } from "@chakra-ui/react"
+import AddSearchModal from "../components/addSearchModal/AddSearchModal"
 
-const Searches = () => (
-    <Box
-        m={4}
-        p={8}
-        bg="gray.50"
-        borderRadius="xl"
-        boxShadow="sm"
-        minH="89vh"
-    >
-        <Flex align="center" mb={4}>
-            <Heading size="4xl">Мои поиски</Heading>
-            <Button
-                colorScheme="teal"
-                size="md"
-                borderRadius="xl"
-                ml={12} 
-            >
-                + Создать поиск
-            </Button>
-        </Flex>
-        <Text color="gray.600">Здесь будут отображаться ваши поисковые запросы и результаты.</Text>
-    </Box>
-)
+const Searches = () => {
+    return (
+        <Box
+            m={4}
+            p={8}
+            bg="gray.50"
+            borderRadius="xl"
+            boxShadow="sm"
+            minH="89vh"
+        >
+            <Flex align="center" mb={4}>
+                <Heading size="4xl">Мои поиски</Heading>
+                <AddSearchModal />
+            </Flex>
+            <Text color="gray.600">Здесь будут отображаться ваши поисковые запросы и результаты.</Text>
+        </Box>
+    )
+}
 
 export default Searches
